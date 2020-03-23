176f6a7c4c55d9a48ca50c84e466415755b3fb01
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2020-03-23 14:56:49 -0700

    - updated sbc-common-components to v.2.1.19 (#48)
    - updated Jest config to fix errors

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   globals: {
     'ts-jest': {
       diagnostics: false,
-      isolatedModules: true
+      isolatedModules: true,
+      babelConfig: true
     }
   },
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
