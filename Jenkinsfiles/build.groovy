9f11b32a21854234f7b86343840d494dd7cc50e9
Kial Jinnah <kialj876@gmail.com>
2020-06-17 15:31:55 -0700

    added in linter
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- Jenkinsfiles/build.groovy
+++ Jenkinsfiles/build.groovy
@@ -106,7 +106,67 @@ if (!run_pipeline) {
     return
 }
 
-node {
+// create NodeJS pod to run verification steps
+def nodejs_label = "jenkins-nodejs-${UUID.randomUUID().toString()}"
+podTemplate(label: nodejs_label, name: nodejs_label, serviceAccount: 'jenkins', cloud: 'openshift', containers: [
+    containerTemplate(
+        name: 'jnlp',
+        image: '172.50.0.2:5000/openshift/jenkins-slave-nodejs:8',
+        resourceRequestCpu: '500m',
+        resourceLimitCpu: '1000m',
+        resourceRequestMemory: '1Gi',
+        resourceLimitMemory: '2Gi',
+        workingDir: '/tmp',
+        command: '',
+        args: '${computer.jnlpmac} ${computer.name}'
+    )
+])
+{
+    node (nodejs_label) {
+        checkout scm
+        dir(CONTEXT_DIRECTORY) {
+            try {
+                sh '''
+                    node -v
+                    npm install
+                '''
+                /* TODO - these do not run correctly in pipeline
+                stage("Run Jest tests") {
+                    def testResults = sh(script: "npm run test:unit-silent", returnStatus: true)
+
+                    echo "Unit tests ran, returned ${testResults}"
+                    if (testResults != 0) {
+                        try {
+                            timeout(time: 1, unit: 'DAYS') {
+                                input message: "Unit tests failed. Continue?", id: "1"
+                            }
+                        } catch (Exception e) {
+                            error('Abort')
+                        }
+                    }
+                }
+                */
+                stage("Check code quality (lint)") {
+                    def lintResults = sh(script: "npm run lint:nofix", returnStatus: true)
+
+                    echo "Linter ran, returned ${lintResults}"
+                    if (lintResults > 0) {
+                        try {
+                            timeout(time: 1, unit: 'DAYS') {
+                                input message: "Linter failed. Continue?", id: "2"
+                            }
+                        } catch (Exception e) {
+                            error('Abort')
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                error('Failure')
+            }
+
+        }
+    }
     stage("Build ${COMPONENT_NAME}-inter") {
       script {
         openshift.withCluster() {
