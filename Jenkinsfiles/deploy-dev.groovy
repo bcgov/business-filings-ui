c393077f16da9c5b3b4460ad18651f04ac3b3ad2
pwei1018 <patrick.wei@gov.bc.ca>
2020-09-17 11:17:45 -0700

    Update CI/CD GHA scripts and nginx config. Clean up the Openshift and Jenkins templates.

--- Jenkinsfiles/deploy-dev.groovy
+++ Jenkinsfiles/deploy-dev.groovy
@@ -21,8 +21,9 @@
 // define constants
 def NAMESPACE = '1rdehl'
 def COMPONENT_NAME = 'business-filings'
-def TAG_NAME = 'prod'
-def SOURCE_TAG = 'test'
+def SOURCE_TAG = 'latest'
+def DEPLOY_TAG = 'dev'
+def PREV_TAG = "${DEPLOY_TAG}-previous"
 
 // define groovy functions
 import groovy.json.JsonOutput
@@ -47,23 +48,59 @@ properties([
 ])
 
 node {
-    stage("Tag ${COMPONENT_NAME}:${TAG_NAME}") {
+    def old_version
+    stage("Deploy ${COMPONENT_NAME}:${DEPLOY_TAG}") {
         script {
             openshift.withCluster() {
+                openshift.withProject("${NAMESPACE}-${DEPLOY_TAG}") {
+                    old_version = openshift.selector('dc', "${COMPONENT_NAME}-${DEPLOY_TAG}").object().status.latestVersion
+                }
+            }
+            openshift.withCluster() {
                 openshift.withProject() {
-                    echo "Tagging ${COMPONENT_NAME}:${TAG_NAME}-previous"
-                    def IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${TAG_NAME}")
-                    echo "IMAGE_HASH: ${IMAGE_HASH}"
-                    openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${TAG_NAME}-previous")
 
-                    echo "Tagging ${COMPONENT_NAME} for deployment to ${TAG_NAME} ..."
+                    echo "Tagging ${COMPONENT_NAME}:${DEPLOY_TAG} to ${DEPLOY_TAG}-previous ..."
+
                     // Don't tag with BUILD_ID so the pruner can do it's job; it won't delete tagged images.
                     // Tag the images for deployment based on the image's hash
+                    def IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${DEPLOY_TAG}")
+                    echo "IMAGE_HASH: ${IMAGE_HASH}"
+                    openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${DEPLOY_TAG}-previous")
+
+                    echo "Tagging ${COMPONENT_NAME} for deployment to ${DEPLOY_TAG} ..."
+
                     IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${SOURCE_TAG}")
                     echo "IMAGE_HASH: ${IMAGE_HASH}"
-                    openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${TAG_NAME}")
+                    openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${DEPLOY_TAG}")
                 }
             }
         }
     }
-}//end node
+    stage("Verify deployment") {
+        sleep 10
+        script {
+            openshift.withCluster() {
+                openshift.withProject("${NAMESPACE}-${DEPLOY_TAG}") {
+                    def new_version = openshift.selector('dc', "${COMPONENT_NAME}-${DEPLOY_TAG}").object().status.latestVersion
+                    if (new_version == old_version) {
+                        echo "New deployment was not triggered."
+                        currentBuild.result = "FAILURE"
+                        return
+                    }
+                    def pod_selector = openshift.selector('pod', [ app:"${COMPONENT_NAME}-${DEPLOY_TAG}" ])
+                    pod_selector.untilEach {
+                        deployment = it.objects()[0].metadata.labels.deployment
+                        echo deployment
+                        if (deployment ==  "${COMPONENT_NAME}-${DEPLOY_TAG}-${new_version}" && it.objects()[0].status.phase == 'Running' && it.objects()[0].status.containerStatuses[0].ready) {
+                            return true
+                        } else {
+                            echo "Pod for new deployment not ready"
+                            sleep 5
+                            return false
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
