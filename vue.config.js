1d045ca542a411cf9a28434324fe80187f08aa8c
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2021-03-02 14:56:01 -0800

    Update vue.config.js
    - updated proxy URL for keycloak config

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
     proxy: {
       // this is needed to prevent a CORS error when running locally
       '/local-keycloak-config-url/*': {
-        target: 'https://dev.bcregistry.ca/business/auth/config/kc/',
+        target: 'https://business-filings-dev.pathfinder.gov.bc.ca/businesses/edit/config/kc/',
         pathRewrite: {
           '/local-keycloak-config-url': ''
         }
