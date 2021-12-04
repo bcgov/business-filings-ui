6efe3d917e32c7398312f5b5b16b7c6a02de8402
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2021-12-03 16:42:12 -0800

    - added plugin to remove existing service workers on users' systems + config (#266)
    - removed plugin to implement Vue PWA + config
    - deleted obsolete manifest file
    - deleted obsolete icons
    - modify service work registration to delete cache
    - deleted obsolete service worker script (now uses script from plugin)
    - app version: 4.2.0

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,41 +1,13 @@
-/* eslint-disable no-console */
-
 import { register } from 'register-service-worker'
 
-const confirmWindowText = 'A new version of the BC Registries application is available. \n\n' +
-                          'Select OK to update now, or Cancel to update later. \n\n' +
-                          'Note: Selecting OK will cause unsaved changes to be lost. ' +
-                          'Select Cancel to save your changes and continue using the ' +
-                          'current version of the application. \n\n' +
-                          'The application will automatically update when you close your browser.'
-
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated (registration) {
-      console.log('New content is available; please refresh.')
-      let confirmationResult = confirm(confirmWindowText)
-      if (confirmationResult) { registration.waiting.postMessage({ action: 'skipWaiting' }) }
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.log('Error during service worker registration:', error)
+  register('service-worker.js', {
+    updated () {
+      // remove older cached content
+      // ref: https://santhoshkumarravi.medium.com/vue-pwa-disable-5463e44b1f7f
+      caches.keys().then(names => {
+        for (let name of names) caches.delete(name)
+      })
     }
   })
 
