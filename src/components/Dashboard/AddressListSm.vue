3c7632fce612282e9b0bea190786a6b206fb969d
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2022-04-22 16:20:33 -0700

    no message

--- src/components/Dashboard/AddressListSm.vue
+++ src/components/Dashboard/AddressListSm.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="address-list-sm">
     <template v-if="isFirm">
-        <firmsAddressList
+        <FirmsAddressList
         :showCompleteYourFilingMessage="showCompleteYourFilingMessage"
         :showGrayedOut="showGrayedOut" />
     </template>
