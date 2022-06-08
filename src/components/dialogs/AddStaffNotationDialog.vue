3e396dea1cc6d0cb16a813d14203fee96956d0ce
Megan <megan.wong@gov.bc.ca>
2022-06-08 14:49:28 -0700

    fix local getters

--- src/components/dialogs/AddStaffNotationDialog.vue
+++ src/components/dialogs/AddStaffNotationDialog.vue
@@ -2,20 +2,19 @@
   <v-dialog v-model="dialog" width="45rem" persistent :attach="attach" content-class="add-notation-dialog">
     <v-card>
       <v-card-title id="dialog-title">
-        <span v-if="administrativeDissolution(name)"
-        id="dialog-title"><strong>{{displayName}}</strong></span>
-        <span v-else-if="putBackOn(name)" id="dialog-title"><strong>Correction - {{displayName}}</strong></span>
+        <span v-if="administrativeDissolution" id="dialog-title"><strong>{{displayName}}</strong></span>
+        <span v-else-if="putBackOn" id="dialog-title"><strong>Correction - {{displayName}}</strong></span>
         <span v-else id="dialog-title"><strong>Add a {{displayName}}</strong> </span>
       </v-card-title>
       <v-card-text>
         <div id="dialog-text" class="dialog-text">
-          <p v-if="administrativeDissolution(name)"> You are about to dissolve
+          <p v-if="administrativeDissolution"> You are about to dissolve
           <strong><span class="text-uppercase">{{getEntityName}}</span>, {{getIdentifier}}</strong> . </p>
-          <p v-if="putBackOn(name)"> You are about to put <strong><span class="text-uppercase">{{getEntityName}}</span>,
+          <p v-if="putBackOn"> You are about to put <strong><span class="text-uppercase">{{getEntityName}}</span>,
           {{getIdentifier}}</strong> back on the register.</p>
         </div>
         <div id="notation-text" class="mb-4 mt-2 pt-4">
-          Enter a {{((administrativeDissolution(name) || putBackOn(name))) ? 'Detail' : displayName}}
+          Enter a {{(administrativeDissolution || putBackOn) ? 'Detail' : displayName}}
           that will appear on the ledger for this entity
         </div>
         <v-form ref="notationFormRef" v-model="notationFormValid" id="notation-form">
@@ -23,7 +22,7 @@
             v-model="notation"
             class="text-input-field mb-2"
             filled
-            :label="(administrativeDissolution(name) || putBackOn(name)) ? 'Add Detail' : displayName"
+            :label="(administrativeDissolution || putBackOn) ? 'Add Detail' : displayName"
             id="notation"
             rows="5"
             :no-resize="true"
@@ -132,13 +131,13 @@ export default class AddStaffNotationDialog extends Mixins(DateMixin, EnumMixin)
   private enableValidation = false
 
   /** Whether this filing is an administrative dissolution */
-  private administrativeDissolution (name: string): boolean {
-    return this.isTypeAdministrativeDissolution({ name })
+  private get administrativeDissolution (): boolean {
+    return this.isTypeAdministrativeDissolution({ name: this.name })
   }
 
   /** Whether this filing is a put back on */
-  private putBackOn (name: string): boolean {
-    return this.isTypePutBackOn({ name })
+  private get putBackOn (): boolean {
+    return this.isTypePutBackOn({ name: this.name })
   }
 
   private get notationRules (): Array<Function> {
