b32d9bd8271d231eb9a22ea266e8762fc2e9ead6
Severin Beauvais <severin.beauvais@gmail.com>
2022-10-14 12:14:36 -0700

    - added some font size CSS variables - small UI tweaks per Yui - app version = 5.7.8

--- src/components/Dashboard/Alerts/MissingInformation.vue
+++ src/components/Dashboard/Alerts/MissingInformation.vue
@@ -1,30 +1,30 @@
 <template>
   <v-expansion-panels v-model="panel" id="missing-information-container">
     <v-expansion-panel class="mb-6">
-      <v-expansion-panel-header hide-actions class="d-flex justify-space-between py-6">
+      <v-expansion-panel-header hide-actions class="d-flex justify-space-between px-6 py-5">
         <h3>
           <v-icon left color="orange darken-2" class="mt-n1">mdi-alert</v-icon>
-          Missing information
+          <span>Missing information</span>
         </h3>
-        <v-btn text color="primary" class="details-btn mt-n1" @click.stop="togglePanel()">
+        <v-btn text color="primary" class="details-btn my-n1" @click.stop="togglePanel()">
           <span color="primary">{{ panel === 0 ? "Hide Details" : "View Details" }}</span>
-          <v-icon right color="primary" size="24px">
+          <v-icon right color="primary">
             {{ panel === 0 ? "mdi-chevron-up" : "mdi-chevron-down" }}
           </v-icon>
         </v-btn>
       </v-expansion-panel-header>
 
       <v-expansion-panel-content>
-        <p>
+        <p class="mb-0">
           BC Registries is missing information about your business (e.g., business start date,
           nature of business, business address, etc.). Please contact BC Registries to input
           any missing business information. Missing information must be entered before you can
           file changes or dissolve this business.
         </p>
-        <p>
+        <p class="mb-0 pt-5">
           If further action is required, please contact BC Registries staff:
         </p>
-        <ContactInfo class="my-4" />
+        <ContactInfo class="pt-5" />
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
@@ -54,14 +54,46 @@ export default class MissingInformation extends Vue {
   pointer-events: none; // disable whole-row expansion
 }
 
+h3 {
+  .v-icon {
+    font-size: $px-18;
+  }
+
+  span {
+    font-size: $px-16;
+  }
+}
+
 .details-btn {
   pointer-events: auto; // enable detail button only
   max-width: fit-content;
-  font-size: $px-14;
   color: $app-blue;
+
+  span {
+    font-size: $px-13;
+  }
+
+  .v-icon {
+    font-size: $px-24 !important;
+  }
+}
+
+// override default expansion panel padding
+:deep(.v-expansion-panel-content__wrap) {
+  padding: 0 1.5rem 1.25rem 1.5rem;
 }
 
 p {
-  font-size: $px-15;
+  font-size: $px-14;
+}
+
+// override contact info sizes
+:deep(.contact-info .contact-container) {
+  .v-icon {
+    font-size: $px-13 !important;
+  }
+  span {
+    font-size: $px-14 !important;
+  }
 }
 </style>
