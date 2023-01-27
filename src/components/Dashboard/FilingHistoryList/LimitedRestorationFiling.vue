0f923b73fed3eb70126eda036b013c57619354da
Jonathan Longe <jlonge@corebox.net>
2023-01-27 09:02:28 -0800

    add missing 'is'

--- src/components/Dashboard/FilingHistoryList/LimitedRestorationFiling.vue
+++ src/components/Dashboard/FilingHistoryList/LimitedRestorationFiling.vue
@@ -4,7 +4,7 @@
 
     <p class="limited-restoration-period .font-15">
       The Company <strong>{{ filing.legalName }}</strong> was successfully
-      restored and active <strong>until {{ expiryDateFriendly }}</strong>.
+      restored and is active <strong>until {{ expiryDateFriendly }}</strong>.
       At the end of the limited restoration period, the company will be automatically dissolved.
       If you require assistance to extend a limited restoration/reinstatement or wish to convert
       your restoration from a limited period to a full restoration, please contact BC Registries staff:
