d6266d1a297282d0b3b35275f87ee029f4306244
pwei1018 <patrick.wei@gov.bc.ca>
2023-04-20 13:46:09 -0700

    Clean up for the new domain name changes.

--- vue.config.js
+++ vue.config.js
@@ -37,13 +37,6 @@ module.exports = {
   publicPath: `${process.env.VUE_APP_PATH}`,
   devServer: {
     proxy: {
-      // this is needed to prevent a CORS error when running locally
-      '/local-keycloak-config-url/*': {
-        target: 'https://dev.bcregistry.ca/business/config/kc/',
-        pathRewrite: {
-          '/local-keycloak-config-url': ''
-        }
-      },
       // this is needed to avoid a PAYBC Not Found error when running locally
       '/status/PAYBC': {
         target: 'https://status-api-dev.apps.silver.devops.gov.bc.ca/api/v1/status/PAYBC',
