24401c21e5b0d6757e25eb512eea591290eace22
JazzarKarim <karim.jazzar@gov.bc.ca>
2023-08-24 11:15:43 -0700

    updated to use BaseAddress from bcrs shared components

--- src/components/common/SummaryOfficeAddresses.vue
+++ src/components/common/SummaryOfficeAddresses.vue
@@ -84,7 +84,7 @@
 
 <script lang="ts">
 import { Component, Mixins, Prop } from 'vue-property-decorator'
-import BaseAddress from 'sbc-common-components/src/components/BaseAddress.vue'
+import { BaseAddress } from '@bcrs-shared-components/base-address'
 import { CommonMixin } from '@/mixins'
 import { AddressIF, OfficeAddressIF } from '@/interfaces'
 
