430bc8b8fc727b3bdebb31e6098f7005c774f43b
JazzarKarim <karim.jazzar@gov.bc.ca>
2023-11-03 11:41:59 -0700

    Updated in response to UXA notes

--- src/components/common/DetailComment.vue
+++ src/components/common/DetailComment.vue
@@ -93,8 +93,9 @@ export default class DetailComment extends Vue {
 
   /** Emits an event with the changed comment (ie, updated v-model). */
   @Emit('input')
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  emitInput (val: string): void {}
+  emitInput (val: string): void {
+    this.detailedComment = val
+  }
 
   /** Emits an event indicating whether or not this component is valid. */
   @Emit('valid')
