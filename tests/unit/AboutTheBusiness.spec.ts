56b2276192c752ec6deee7280cbdfc8c26820fc0
leodube-aot <leo.dube@aot-technologies.com>
2023-11-07 14:35:34 -0800

    Incorporate Sev's test for AboutTheBusiness

--- tests/unit/AboutTheBusiness.spec.ts
+++ tests/unit/AboutTheBusiness.spec.ts
@@ -9,29 +9,38 @@ Vue.use(Vuetify)
 const vuetify = new Vuetify({})
 
 describe('About The Business component', () => {
-  it('handles empty data', async () => {
+  it('handles empty data', () => {
     const wrapper = mount(AboutTheBusiness, {
-      vuetify,
-      propsData: { data: EmptyAgmExtEval }
+      propsData: { data: EmptyAgmExtEval },
+      vuetify
     })
-    await Vue.nextTick()
 
     expect(wrapper.find('#entity-in-good-standing').text()).toBe('No')
     expect(wrapper.find('#entity-date-of-incorporation').text()).toBe('')
+
+    wrapper.destroy()
   })
 
-  it('displays business info properly', async () => {
+  it('displays business info properly', () => {
     const wrapper = mount(AboutTheBusiness, {
-      vuetify,
       propsData: {
         data: {
           ...EmptyAgmExtEval,
           isGoodStanding: true,
           incorporationDate: new Date('2023-12-31T08:00:00.000Z')
         } as AgmExtEvalIF
-      }
+      },
+      vuetify
     })
-    await Vue.nextTick()
+
+    // verify component displays properly
+    expect(wrapper.find('.v-card').attributes('id')).toBe('about-the-business')
+    expect(wrapper.find('header i').attributes('class')).toContain('mdi-domain')
+    expect(wrapper.find('header h2').text()).toBe('About the Business')
+
+    const rows = wrapper.findAll('.content > .row')
+    expect(rows.at(0).find('.col-sm-3').text()).toBe('Business in Good Standing')
+    expect(rows.at(1).find('.col-sm-3').text()).toBe('Date of Incorporation')
 
     // verify displayed text
     expect(wrapper.find('#entity-in-good-standing').text()).toBe('Yes')
@@ -39,5 +48,7 @@ describe('About The Business component', () => {
       .toBe(DateUtilities.dateToPacificDate(
         new Date('2023-12-31T08:00:00.000Z'), true
       ))
+
+    wrapper.destroy()
   })
 })
