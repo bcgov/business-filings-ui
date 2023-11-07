e4ffc2d8ba9e520383c4fa32d53256edff2eac4e
leodube-aot <leo.dube@aot-technologies.com>
2023-11-07 14:25:37 -0800

    Update AboutTheBusiness tests

--- tests/unit/AboutTheBusiness.spec.ts
+++ tests/unit/AboutTheBusiness.spec.ts
@@ -1,50 +1,34 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import VueRouter from 'vue-router'
-import { shallowMount } from '@vue/test-utils'
-import { createPinia, setActivePinia } from 'pinia'
-import { useBusinessStore } from '@/stores'
+import { mount } from '@vue/test-utils'
 import AboutTheBusiness from '@/components/AgmExtension/AboutTheBusiness.vue'
-import mockRouter from './mockRouter'
-import { AgmExtEvalIF } from '@/interfaces'
+import { AgmExtEvalIF, EmptyAgmExtEval } from '@/interfaces'
 import { DateUtilities } from '@/services'
 
 Vue.use(Vuetify)
-Vue.use(VueRouter)
-
 const vuetify = new Vuetify({})
-setActivePinia(createPinia())
-const businessStore = useBusinessStore()
 
 describe('About The Business component', () => {
-  const router = mockRouter.mock()
-
   it('handles empty data', async () => {
-    const wrapper = shallowMount(AboutTheBusiness, {
+    const wrapper = mount(AboutTheBusiness, {
       vuetify,
-      router,
-      propsData: { data: null }
+      propsData: { data: EmptyAgmExtEval }
     })
     await Vue.nextTick()
 
-    expect(wrapper.find('#entity-in-good-standing').exists()).toBe(false)
-    expect(wrapper.find('#entity-date-of-incorporation').exists()).toBe(false)
+    expect(wrapper.find('#entity-in-good-standing').text()).toBe('No')
+    expect(wrapper.find('#entity-date-of-incorporation').text()).toBe('')
   })
 
   it('displays business info properly', async () => {
-    // set store properties
-    businessStore.setGoodStanding(true)
-    businessStore.setFoundingDate('1971-05-12T00:00:00-00:00')
-
-    // mount the component and wait for everything to stabilize
-    const wrapper = shallowMount(AboutTheBusiness, {
+    const wrapper = mount(AboutTheBusiness, {
       vuetify,
-      router,
       propsData: {
         data: {
-          isGoodStanding: businessStore.isGoodStanding,
-          incorporationDate: businessStore.getFoundingDate
-        } as unknown as AgmExtEvalIF
+          ...EmptyAgmExtEval,
+          isGoodStanding: true,
+          incorporationDate: new Date('2023-12-31T08:00:00.000Z')
+        } as AgmExtEvalIF
       }
     })
     await Vue.nextTick()
@@ -52,6 +36,8 @@ describe('About The Business component', () => {
     // verify displayed text
     expect(wrapper.find('#entity-in-good-standing').text()).toBe('Yes')
     expect(wrapper.find('#entity-date-of-incorporation').text())
-      .toBe(DateUtilities.dateToPacificDate(businessStore.getFoundingDate, true))
+      .toBe(DateUtilities.dateToPacificDate(
+        new Date('2023-12-31T08:00:00.000Z'), true
+      ))
   })
 })
