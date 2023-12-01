8ef6dbe2cd68c83ed3f23c4edd83e17306e16581
Akiff Manji <amanji@petridish.dev>
2023-12-01 21:04:16 +0000

    chore: address code review comments and bump package version
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- src/components/DigitalCredentials/dialogs/ConfirmCredentialsTermsofUseDialog.vue
+++ src/components/DigitalCredentials/dialogs/ConfirmCredentialsTermsofUseDialog.vue
@@ -9,16 +9,7 @@
   >
     <v-card class="pa-5">
       <v-card-text>
-        <p
-          id="dialog-title"
-          class="warning-title"
-        />
-        <p
-          id="dialog-text"
-          class="warning-text"
-        >
-          <CredentialsTermsOfUse />
-        </p>
+        <CredentialsTermsOfUse />
       </v-card-text>
 
       <v-card-actions>
@@ -51,58 +42,44 @@
 
 <script lang="ts">
 import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
-import CredentialsTermsOfUse from './CredentialsTermsOfUse.vue'
+import CredentialsTermsOfUse from '@/components/DigitalCredentials/CredentialsTermsOfUse.vue'
 
-  @Component({
-    components: {
-      CredentialsTermsOfUse
-    }
-  })
+@Component({
+  components: {
+    CredentialsTermsOfUse
+  }
+})
 export default class ConfirmCredentialsTermsOfUseDialog extends Vue {
-    /** Prop to display the dialog. */
-    @Prop({ default: false }) readonly dialog!: boolean
+  /** Prop to display the dialog. */
+  @Prop({ default: false }) readonly dialog!: boolean
 
-    /** Prop to provide attachment selector. */
-    @Prop({ default: '' }) readonly attach!: string
+  /** Prop to provide attachment selector. */
+  @Prop({ default: '' }) readonly attach!: string
 
-    // Pass click events to parent.
-    @Emit() close (): void {
-      return undefined
-    }
-    @Emit() proceed (): void {
-      return undefined
-    }
+  // Pass click events to parent.
+  @Emit() close (): void {
+    return undefined
+  }
+  @Emit() proceed (): void {
+    return undefined
+  }
 }
 </script>
 
-  <style lang="scss" scoped>
-  @import '@/assets/styles/theme.scss';
+<style lang="scss" scoped>
+@import '@/assets/styles/theme.scss';
 
-  .close-btn {
-    position: absolute;
-    right: $px-32;
-  }
+.close-btn {
+  position: absolute;
+  right: $px-32;
+}
 
-  .action-btn {
-    min-height: 44px;
-  }
+.action-btn {
+  min-height: 44px;
+}
 
-  .warning-title {
-    font-size: $px-24;
-    font-weight: bold;
-    color: $gray9;
-    line-height: $px-34;
-  }
-
-  .warning-text {
-    font-weight: normal;
-    font-size: $px-16;
-    color: $gray7;
-    line-height: $px-24;
-  }
-
-  // Vuetify overrides
-  :deep(.v-dialog .v-card .v-card__text) {
-    padding-top: 0 !important;
-  }
-  </style>
+// Vuetify overrides
+:deep(.v-dialog .v-card .v-card__text) {
+  padding-top: 0 !important;
+}
+</style>
