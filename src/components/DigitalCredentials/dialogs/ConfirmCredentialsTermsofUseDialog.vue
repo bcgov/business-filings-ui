01fd5c094e94e774ba084bb76946af19b1fcc419
Akiff Manji <amanji@petridish.dev>
2023-12-04 14:18:21 -0800

    feat: digital credentials TOU (#597)
    * feat: Terms of Use for digital credentials
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    * chore: address code review comments and bump package version
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    * chore: address code review comments
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    * chore: address code review comments
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    * chore: address code review comments
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    ---------
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- src/components/DigitalCredentials/dialogs/ConfirmCredentialsTermsofUseDialog.vue
+++ src/components/DigitalCredentials/dialogs/ConfirmCredentialsTermsofUseDialog.vue
@@ -2,25 +2,14 @@
   <v-dialog
     id="confirm-credentials-terms-of-use-dialog"
     v-model="dialog"
-    width="45rem"
+    width="60rem"
     persistent
     :attach="attach"
     content-class="confirm-credentials-terms-of-use-dialog"
   >
     <v-card class="pa-5">
       <v-card-text>
-        <p
-          id="dialog-title"
-          class="warning-title"
-        >
-          Digital Business Card Terms of Use
-        </p>
-        <p
-          id="dialog-text"
-          class="warning-text"
-        >
-          I promise to be a good person and not use this credential for evil.
-        </p>
+        <CredentialsTermsOfUse />
       </v-card-text>
 
       <v-card-actions>
@@ -53,53 +42,44 @@
 
 <script lang="ts">
 import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
+import CredentialsTermsOfUse from '@/components/DigitalCredentials/CredentialsTermsOfUse.vue'
 
-  @Component({})
+@Component({
+  components: {
+    CredentialsTermsOfUse
+  }
+})
 export default class ConfirmCredentialsTermsOfUseDialog extends Vue {
-    /** Prop to display the dialog. */
-    @Prop({ default: false }) readonly dialog!: boolean
+  /** Prop to display the dialog. */
+  @Prop({ default: false }) readonly dialog!: boolean
 
-    /** Prop to provide attachment selector. */
-    @Prop({ default: '' }) readonly attach!: string
+  /** Prop to provide attachment selector. */
+  @Prop({ default: '' }) readonly attach!: string
 
-    // Pass click events to parent.
-    @Emit() close (): void {
-      return undefined
-    }
-    @Emit() proceed (): void {
-      return undefined
-    }
+  // Pass click events to parent.
+  @Emit() close (): void {
+    return undefined
+  }
+  @Emit() proceed (): void {
+    return undefined
+  }
 }
 </script>
 
-  <style lang="scss" scoped>
-  @import '@/assets/styles/theme.scss';
+<style lang="scss" scoped>
+@import '@/assets/styles/theme.scss';
 
-  .close-btn {
-    position: absolute;
-    right: $px-32;
-  }
+.close-btn {
+  position: absolute;
+  right: $px-32;
+}
 
-  .action-btn {
-    min-height: 44px;
-  }
+.action-btn {
+  min-height: 44px;
+}
 
-  .warning-title {
-    font-size: $px-24;
-    font-weight: bold;
-    color: $gray9;
-    line-height: $px-34;
-  }
-
-  .warning-text {
-    font-weight: normal;
-    font-size: $px-16;
-    color: $gray7;
-    line-height: $px-24;
-  }
-
-  // Vuetify overrides
-  :deep(.v-dialog .v-card .v-card__text) {
-    padding-top: 0 !important;
-  }
-  </style>
+// Vuetify overrides
+:deep(.v-dialog .v-card .v-card__text) {
+  padding-top: 0 !important;
+}
+</style>
