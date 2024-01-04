453f34284de377299b064ad28e822c5e00dd9e37
Kevin Zhang <kevinzhang940420@gmail.com>
2024-01-04 12:13:05 -0800

    fix small

--- src/stores/filingHistoryListStore.ts
+++ src/stores/filingHistoryListStore.ts
@@ -30,8 +30,9 @@ export const useFilingHistoryListStore = defineStore('filingHistoryList', {
       return state.filings.filter(filing => {
         // safety check for required fields
         if (!filing.name || !filing.displayName || !filing.effectiveDate || !filing.submittedDate || !filing.status || !filing.displayLedger) {
-          // eslint-disable-next-line no-console
-          console.log('Invalid filing =', filing)
+          if (filing.displayLedger)
+            // eslint-disable-next-line no-console
+            console.log('Invalid filing =', filing)
           return false
         }
         return true
