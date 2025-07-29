bcgov/business-filings-ui

commit cb77150b46f6c31f903227cf293b53ef555a8d0e

27186 refactor roles to permissions (#756)
* 27186-initial refactor commit

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* Removing Dashboard and related components

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186-Update to App.Vue to remove dashboard related code, small auth changes

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 - IsStaffRole changes, start of test updates.

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 Unit test fixes, fixing loadAccountInformation

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 - remove premium accounts

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 removing unused imports identified by lint

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 removing redundant code re: SonarQube

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* Updates to address SonarQube issues

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 - Initial combined suggested fixes/changes

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 - various suggested updates, refactor to use keycloak roles

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186-Implementing public user permissions

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 - Updating some roles, addressing todo's

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* removing AccountInformationIF, adding checks in for file and pay, updating tests

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186-making some updates as suggested in PR

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186 - update to replace JSDoc comments

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

* 27186-Removing unused permission, incrementing full version for major change

Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

src/stores/rootStore.ts
@@ -1,9 +1,19 @@
 import { defineStore } from 'pinia'
-import { CorpTypeCd, FilingStatus, FilingSubTypes } from '@/enums'
+import { AuthorizationRoles, CorpTypeCd, FilingStatus, FilingSubTypes } from '@/enums'
 import { FilingTypes } from '@bcrs-shared-components/enums'
-import { ApiTaskIF, DissolutionConfirmationResourceIF, FilingDataIF, OfficeAddressIF, PartyIF,
-  RootStateIF, TodoListResourceIF, IsoDatePacific, StateFilingIF } from '@/interfaces'
+import {
+  ApiTaskIF,
+  DissolutionConfirmationResourceIF,
+  FilingDataIF,
+  OfficeAddressIF,
+  PartyIF,
+  RootStateIF,
+  TodoListResourceIF,
+  IsoDatePacific,
+  StateFilingIF
+} from '@/interfaces'
 import { DateUtilities, EnumUtilities, LegalServices } from '@/services'
+
 import { useBusinessStore } from './businessStore'
 import { useFilingHistoryListStore } from './filingHistoryListStore'
 
@@ -14,8 +24,6 @@ export const useRootStore = defineStore('root', {
     currentJsDate: null,
     bootstrapFilingStatus: null,
     bootstrapFilingType: null,
-    keycloakRoles: [],
-    noRedirect: false,
     stateFiling: null,
     userKeycloakGuid: null,
     businessEmail: null,
@@ -86,34 +94,10 @@ export const useRootStore = defineStore('root', {
     },
 
     /**
-     * The roles from the Keycloak token (JWT).
-     * @deprecated Use `authenticationStore.getKeycloakRoles` instead.
+     * The user's roles from the Keycloak token.
      */
-    getKeycloakRoles (state: RootStateIF): Array<string> {
-      return state.keycloakRoles
-    },
-
-    /** Whether initial route specified "noRedirect" URL parameter. */
-    isNoRedirect (state: RootStateIF): boolean {
-      return state.noRedirect
-    },
-
-    /**
-     * Is True if Staff role is set.
-     * @deprecated Use `authenticationStore.isRoleStaff` instead.
-     */
-    isRoleStaff (state: RootStateIF): boolean {
-      return state.keycloakRoles.includes('staff')
-    },
-
-    /** Is True if app permissions includes Edit role. */
-    isRoleEdit (state: RootStateIF): boolean {
-      return state.authRoles.includes('edit')
-    },
-
-    /** Is True if app permission includes View role. */
-    isRoleView (state: RootStateIF): boolean {
-      return state.authRoles.includes('view')
+    getAuthRoles (): Array<AuthorizationRoles> {
+      return this.authRoles
     },
 
     /** Is True if business is pending dissolution. */
@@ -123,28 +107,6 @@ export const useRootStore = defineStore('root', {
 
     getBootstrapFilingStatus (state: RootStateIF): FilingStatus {
       return state.bootstrapFilingStatus
-    },
-
-    /** Whether this is an amalgamation bootstrap filing that belongs in the Todo List. */
-    isAmalgamationTodo (state: RootStateIF): boolean {
-      return (
-        state.bootstrapFilingType === FilingTypes.AMALGAMATION_APPLICATION &&
-        (
-          this.getBootstrapFilingStatus === FilingStatus.DRAFT ||
-          this.getBootstrapFilingStatus === FilingStatus.PENDING
-        )
-      )
-    },
-
-    /** Whether this is an amalgamation bootstrap filing that belongs in the Filing History List. */
-    isAmalgamationFiling (state: RootStateIF): boolean {
-      return (
-        state.bootstrapFilingType === FilingTypes.AMALGAMATION_APPLICATION &&
-        (
-          this.getBootstrapFilingStatus === FilingStatus.COMPLETED ||
-          this.getBootstrapFilingStatus === FilingStatus.PAID
-        )
-      )
     },
 
     /** Whether this is a continuation in bootstrap filing that belongs in the Todo List. */
@@ -222,38 +184,6 @@ export const useRootStore = defineStore('root', {
           this.getBootstrapFilingStatus === FilingStatus.PAID
         )
       )
-    },
-
-    /** Is True if this is a bootstrap todo item and should be displayed in the Todo List. */
-    isBootstrapTodo (): boolean {
-      return (
-        this.isAmalgamationTodo ||
-        this.isContinuationInTodo ||
-        this.isIncorporationApplicationTodo ||
-        this.isRegistrationTodo
-      )
-    },
-
-    /** Is True if this is a bootstrap pending item and should be displayed in the Pending List. */
-    isBootstrapPending (): boolean {
-      return (
-        this.isContinuationInPending
-      )
-    },
-
-    /** Is True if this is a bootstrap filing item and should be displayed in the Filing History List. */
-    isBootstrapFiling (): boolean {
-      return (
-        this.isAmalgamationFiling ||
-        this.isContinuationInFiling ||
-        this.isIncorporationApplicationFiling ||
-        this.isRegistrationFiling
-      )
-    },
-
-    /** The Name Request (may be null). */
-    getNameRequest (state: RootStateIF): any {
-      return (state.nameRequest)
     },
 
     /** Whether to show the Fetching Data spinner. */
@@ -435,15 +365,6 @@ export const useRootStore = defineStore('root', {
       this.stateFiling = stateFilingResponse
     },
 
-    /** Set the roles from the Keycloak token (JWT). */
-    setKeycloakRoles (keycloakRoles: Array<string>) {
-      this.keycloakRoles = keycloakRoles
-    },
-
-    setNoRedirect (val: boolean) {
-      this.noRedirect = val
-    },
-
     setUserKeycloakGuid (userKeycloakGuid: string) {
       this.userKeycloakGuid = userKeycloakGuid
     },
@@ -453,7 +374,7 @@ export const useRootStore = defineStore('root', {
     },
 
     /** Set the app permissions. */
-    setAuthRoles (authRoles: Array<string>) {
+    setAuthRoles (authRoles: Array<AuthorizationRoles>) {
       this.authRoles = authRoles
     },
 

src/utils/set-base-route-and-business-id.ts
@@ -9,25 +9,21 @@ export function setBaseRouteAndBusinessId (pathname: string, processEnvBaseUrl: 
     throw new Error('Missing environment variables')
   }
 
-  // get Business ID / Temp Reg Number and validate that it looks OK
+  // get Business ID and validate that it looks OK
   // it should be first token after Base URL in Pathname
-  // FUTURE: improve Business ID / Temp Reg Number validation
   const id = pathname.replace(processEnvBaseUrl, '').split('/', 1)[0]
   const businessIdRegex = /^(BC|C|CP|FM)\d{7}$/
   if (businessIdRegex.test(id)) {
     sessionStorage.setItem('BUSINESS_ID', id)
-    // ensure we don't already have a Temp Reg Number in scope
-    sessionStorage.removeItem('TEMP_REG_NUMBER')
-  } else if (id?.startsWith('T')) {
-    sessionStorage.setItem('TEMP_REG_NUMBER', id)
-    // ensure we don't already have a Business ID in scope
-    sessionStorage.removeItem('BUSINESS_ID')
   } else {
-    throw new Error('Missing or invalid Business ID or Temporary Registration Number.')
+    // Make sure we don't have any previously set values
+    // Handle no business id issue in main.ts
+    sessionStorage.removeItem('BUSINESS_ID')
+    throw new Error('Missing or invalid Business Id.')
   }
 
   // set Base for Vue Router
-  // eg, "/business/CPxxx/" or "/business/Txxx/"
+  // eg, "/business/CPxxx/"
   const vueRouterBase = processEnvBaseUrl + id + '/'
   sessionStorage.setItem('VUE_ROUTER_BASE', vueRouterBase)
 

commit 58de498c511e424a0ba09377401af0b2bed49118

29817 - commit to fix issue with NOW introduced with changes from Commit cb77150
Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

src/App.vue
@@ -99,7 +99,7 @@ import {
 import { ConfigJson } from '@/resources'
 import { BreadcrumbMixin, CommonMixin, DateMixin, DirectorMixin, FilingMixin, NameRequestMixin }
   from '@/mixins'
-import { AuthServices, LegalServices } from '@/services/'
+import { AuthServices, EnumUtilities, LegalServices } from '@/services/'
 import {
   AccountInformationIF,
   ApiFilingIF,
@@ -107,16 +107,18 @@ import {
   DocumentIF,
   NameRequestIF,
   OfficeAddressIF,
-  PartyIF
+  PartyIF,
+  TaskTodoIF
 } from '@/interfaces'
 import { BreadcrumbIF } from '@bcrs-shared-components/interfaces'
 import { AuthorizationRoles, AuthorizedActions, FilingStatus, NameRequestStates, NigsMessage, Routes } from '@/enums'
-import { CorpTypeCd }
+import { CorpTypeCd, GetCorpFullDescription, GetCorpNumberedDescription }
   from '@bcrs-shared-components/corp-type-module'
 import { SessionStorageKeys } from 'sbc-common-components/src/util/constants'
 import { useAuthenticationStore, useBusinessStore, useConfigurationStore, useFilingHistoryListStore, useRootStore }
   from './stores'
 import { sleep } from './utils'
+import { FilingTypes } from '@bcrs-shared-components/enums'
 
 @Component({
   components: {
@@ -181,6 +183,9 @@ export default class App extends Mixins(
 
   // root store references
   @Getter(useRootStore) isAuthorizationStatus!: boolean
+  @Getter(useRootStore) isBootstrapFiling!: boolean
+  @Getter(useRootStore) isBootstrapPending!: boolean
+  @Getter(useRootStore) isBootstrapTodo!: boolean
   @Getter(useRootStore) showFetchingDataSpinner!: boolean
   @Getter(useRootStore) showStartingAmalgamationSpinner!: boolean
 
@@ -303,6 +308,8 @@ export default class App extends Mixins(
   @Action(useRootStore) loadStateFiling!: () => Promise<void>
   @Action(useRootStore) setAccountInformation!: (x: AccountInformationIF) => void
   @Action(useRootStore) setAuthorizedActions!: (x: Array<AuthorizedActions>) => void
+  @Action(useRootStore) setBootstrapFilingStatus!: (x: FilingStatus) => void
+  @Action(useRootStore) setBootstrapFilingType!: (x: FilingTypes) => void
   @Action(useRootStore) setBusinessAddress!: (x: OfficeAddressIF) => void
   @Action(useRootStore) setBusinessEmail!: (x: string) => void
   @Action(useRootStore) setBusinessPhone!: (x: string) => void
@@ -366,6 +373,10 @@ export default class App extends Mixins(
     // fetch user info and update Launch Darkly
     this.setupLaunchDarkly()
 
+    // Business id safety check
+    if (!this.businessId && !this.tempRegNumber) {
+      throw new Error('Missing Business Id or Temporary Registration Number')
+    }
     // is this a business entity?
     if (this.businessId) {
       try {
@@ -379,6 +390,28 @@ export default class App extends Mixins(
         // At this point the system doesn't know why it's incomplete.
         // Since this is not an expected behaviour, report this.
         Sentry.captureException(error)
+      }
+    }
+    // is this a bootstrap filing? (eg, incorporation/registration/amalgamation/continuation)
+    if (this.tempRegNumber) {
+      try {
+        this.nameRequestInvalidType = null // reset for new fetches
+
+        // fetch the bootstrap filing and store the bootstrap item
+        const response = await LegalServices.fetchBootstrapFiling(this.tempRegNumber)
+        this.storeBootstrapItem(response)
+
+        // if it is a todo or a pending filing, and it has a NR, load it
+        // (this is to display the NR details in the Todo List/Pending List)
+        if ((this.isBootstrapTodo || this.isBootstrapPending) && this.localNrNumber) {
+          const nr = await LegalServices.fetchNameRequest(this.localNrNumber)
+          this.storeNrData(nr, response)
+        }
+
+        this.dataLoaded = true
+      } catch (error) {
+        console.log(error) // eslint-disable-line no-console
+        this.nameRequestInvalidDialog = true
       }
     }
   }
@@ -520,6 +553,168 @@ export default class App extends Mixins(
     } else {
       throw new Error('Invalid entity contact info')
     }
+  }
+  /**
+   * Verifies and stores a bootstrap item's data to make this UI (entity dashboard)
+   * look like a business.
+   */
+  storeBootstrapItem (response: any): void {
+    const filing = response?.filing
+    const filingName = filing.header?.name as FilingTypes
+    const status = filing.header.status as FilingStatus
+    const foundingDate = filing.header?.effectiveDate || null // use the FE date as the founding date
+    const email =
+        filing.incorporationApplication?.contactPoint?.email ||
+        filing.amalgamationApplication?.contactPoint?.email ||
+        filing.continuationIn?.contactPoint?.email ||
+        filing.registration?.contactPoint?.email || null
+
+    if (!filing || !filing.business || !filing.header || !filingName || !status) {
+      throw new Error(`Invalid boostrap filing - missing required property = ${filing}`)
+    }
+
+    // special check for amalgamation application
+    if (filingName === FilingTypes.AMALGAMATION_APPLICATION && !filing.amalgamationApplication.type) {
+      throw new Error('Invalid bootstrap filing - missing amalgamation type')
+    }
+
+    // NB: different object from actual NR
+    const nameRequest = filing[filingName].nameRequest as {
+      legalName?: string
+      legalType: CorpTypeCd
+      nrNumber: string
+    }
+    if (!nameRequest) {
+      throw new Error('Invalid bootstrap filing - missing name request object')
+    }
+
+    // verify that this is a supported entity type
+    const legalType = nameRequest.legalType
+    if (!legalType || !this.supportedEntityTypes.includes(legalType)) {
+      throw new Error(`Invalid bootstrap filing - missing or unsupported legal type = ${legalType}`)
+    }
+
+    // store business info
+    this.setBootstrapFilingStatus(status)
+    this.setBootstrapFilingType(filingName)
+    this.setIdentifier(this.tempRegNumber)
+    this.setLegalType(legalType)
+    this.setGoodStanding(true) // draft apps are always in good standing
+    this.setFoundingDate(foundingDate)
+    this.setBusinessEmail(email)
+
+    // save local NR Number if present
+    if (nameRequest.nrNumber) this.localNrNumber = nameRequest.nrNumber
+
+    // store Legal Name if present
+    // special case to identify numbered amalgamations
+    if (filingName === FilingTypes.AMALGAMATION_APPLICATION) {
+      this.setLegalName(nameRequest.legalName || 'Numbered Amalgamated Company')
+    } else { this.setLegalName(nameRequest.legalName || GetCorpNumberedDescription(this.getLegalType)) }
+
+    // store the bootstrap item in the right list
+    if (this.isBootstrapTodo) this.storeBootstrapTodo(response)
+    else if (this.isBootstrapPending) this.storeBootstrapPending(response)
+    else if (this.isBootstrapFiling) this.storeBootstrapFiling(response)
+    else throw new Error(`Invalid boostrap filing - not a task or pending or filing = ${filing}`)
+  }
+
+  /** Stores bootstrap item in the Todo List. */
+  storeBootstrapTodo (response: any): void {
+    const filing = response.filing as TaskTodoIF
+    // NB: these were already validated in storeBootstrapItem()
+    const header = filing.header
+    const data = filing[header.name]
+    const status = header.status
+
+    const description = GetCorpFullDescription(data.nameRequest.legalType)
+    const dba = this.isEntitySoleProp ? ' / Doing Business As (DBA) ' : ' '
+    const filingName = EnumUtilities.filingTypeToName(header.name, null, data.type, status)
+
+    // save display name for later
+    filing.displayName = EnumUtilities.isTypeAmalgamationApplication(header)
+      ? filingName
+      : `${description}${dba}${filingName}`
+
+    // add this as a task item
+    const taskItem: ApiTaskIF = {
+      enabled: true,
+      order: 1,
+      task: { filing }
+    }
+    this.setTasks([taskItem])
+  }
+
+  /** Stores bootstrap item in the Pending List. */
+  storeBootstrapPending (response: any): void {
+    const filing = response.filing as TaskTodoIF
+    // NB: these were already validated in storeBootstrapItem()
+    const header = filing.header
+    const data = filing[header.name]
+    const status = header.status
+
+    // set addresses
+    this.storeAddresses({ data: data.offices || [] })
+
+    // set parties
+    this.storeParties({ data: { parties: data.parties || [] } })
+
+    const description = GetCorpFullDescription(data.nameRequest.legalType)
+    const filingName = EnumUtilities.filingTypeToName(header.name, null, data.type, status)
+
+    // save display name for later
+    filing.displayName = `${description} ${filingName}`
+
+    // add this as a pending item
+    this.setPendingsList([filing])
+  }
+
+  /** Stores bootstrap item in the Filing History List. */
+  storeBootstrapFiling (response: any): void {
+    const filing = response.filing as TaskTodoIF
+    // NB: these were already validated in storeBootstrapItem()
+    const header = filing.header
+    const data = filing[header.name]
+    const status = header.status
+
+    // set addresses
+    this.storeAddresses({ data: data.offices || [] })
+
+    // set parties
+    this.storeParties({ data: { parties: data.parties || [] } })
+
+    const description = GetCorpFullDescription(data.nameRequest.legalType)
+    const filingName = EnumUtilities.filingTypeToName(header.name, null, data.type, status)
+    const displayName = EnumUtilities.isTypeAmalgamationApplication(header)
+      ? filingName
+      : `${description} ${filingName}`
+
+    // add this as a filing item
+    const filingItem = {
+      availableOnPaperOnly: header.availableOnPaperOnly,
+      businessIdentifier: filing.business.identifier || this.getIdentifier,
+      commentsCount: response.commentsCount,
+      commentsLink: response.commentsLink,
+      displayLedger: response.displayLedger,
+      displayName,
+      documentsLink: response.documentsLink,
+      effectiveDate: this.apiToUtcString(header.effectiveDate),
+      filingId: header.filingId,
+      filingLink: response.filingLink,
+      filingSubType: data.type,
+      isFutureEffective: header.isFutureEffective,
+      name: header.name,
+      status: header.status,
+      submittedDate: this.apiToUtcString(header.date),
+      submitter: header.submitter,
+      data: {
+        applicationDate: this.dateToYyyyMmDd(this.apiToDate(header.date)),
+        legalFilings: [header.name],
+        order: data.courtOrder
+      },
+      latestReviewComment: header.latestReviewComment
+    } as ApiFilingIF
+    this.setFilings([filingItem])
   }
 
   storeNrData (nr: NameRequestIF, app: any): void {

src/stores/rootStore.ts
@@ -111,6 +111,28 @@ export const useRootStore = defineStore('root', {
       return state.bootstrapFilingStatus
     },
 
+    /** Whether this is an amalgamation bootstrap filing that belongs in the Todo List. */
+    isAmalgamationTodo (state: RootStateIF): boolean {
+      return (
+        state.bootstrapFilingType === FilingTypes.AMALGAMATION_APPLICATION &&
+        (
+          this.getBootstrapFilingStatus === FilingStatus.DRAFT ||
+          this.getBootstrapFilingStatus === FilingStatus.PENDING
+        )
+      )
+    },
+
+    /** Whether this is an amalgamation bootstrap filing that belongs in the Filing History List. */
+    isAmalgamationFiling (state: RootStateIF): boolean {
+      return (
+        state.bootstrapFilingType === FilingTypes.AMALGAMATION_APPLICATION &&
+        (
+          this.getBootstrapFilingStatus === FilingStatus.COMPLETED ||
+          this.getBootstrapFilingStatus === FilingStatus.PAID
+        )
+      )
+    },
+
     /** Whether this is a continuation in bootstrap filing that belongs in the Todo List. */
     isContinuationInTodo (state: RootStateIF): boolean {
       return (
@@ -186,6 +208,37 @@ export const useRootStore = defineStore('root', {
           this.getBootstrapFilingStatus === FilingStatus.PAID
         )
       )
+    },
+    /** Is True if this is a bootstrap todo item and should be displayed in the Todo List. */
+    isBootstrapTodo (): boolean {
+      return (
+        this.isAmalgamationTodo ||
+        this.isContinuationInTodo ||
+        this.isIncorporationApplicationTodo ||
+        this.isRegistrationTodo
+      )
+    },
+
+    /** Is True if this is a bootstrap pending item and should be displayed in the Pending List. */
+    isBootstrapPending (): boolean {
+      return (
+        this.isContinuationInPending
+      )
+    },
+
+    /** Is True if this is a bootstrap filing item and should be displayed in the Filing History List. */
+    isBootstrapFiling (): boolean {
+      return (
+        this.isAmalgamationFiling ||
+        this.isContinuationInFiling ||
+        this.isIncorporationApplicationFiling ||
+        this.isRegistrationFiling
+      )
+    },
+
+    /** The Name Request (may be null). */
+    getNameRequest (state: RootStateIF): any {
+      return (state.nameRequest)
     },
 
     /** Whether to show the Fetching Data spinner. */

src/utils/set-base-route-and-business-id.ts
@@ -15,6 +15,12 @@ export function setBaseRouteAndBusinessId (pathname: string, processEnvBaseUrl: 
   const businessIdRegex = /^(BC|C|CP|FM)\d{7}$/
   if (businessIdRegex.test(id)) {
     sessionStorage.setItem('BUSINESS_ID', id)
+    // ensure we don't already have a Temp Reg Number in scope
+    sessionStorage.removeItem('TEMP_REG_NUMBER')
+  } else if (id?.startsWith('T')) {
+    sessionStorage.setItem('TEMP_REG_NUMBER', id)
+    // ensure we don't already have a Business ID in scope
+    sessionStorage.removeItem('BUSINESS_ID')
   } else {
     // Make sure we don't have any previously set values
     // Handle no business id issue in main.ts
@@ -23,7 +29,7 @@ export function setBaseRouteAndBusinessId (pathname: string, processEnvBaseUrl: 
   }
 
   // set Base for Vue Router
-  // eg, "/business/CPxxx/"
+  // eg, "/business/CPxxx/" or "/business/Txxxxx/"
   const vueRouterBase = processEnvBaseUrl + id + '/'
   sessionStorage.setItem('VUE_ROUTER_BASE', vueRouterBase)
 