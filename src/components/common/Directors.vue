0ce50fe7a4006a3d950596024acc7855199b24b3
Megan Wong <Megan.a.wong@gov.bc.ca>
2025-08-01 10:41:20 -0700

    29908 - Fix saveEditDirector logic

--- src/components/common/Directors.vue
+++ src/components/common/Directors.vue
@@ -1355,7 +1355,7 @@ export default class Directors extends Mixins(CommonMixin, DateMixin, DirectorMi
     // Legal name confirmation checkbox
     this.showErrors = this.editFormShowHide.showName && !this.legalNameConfirmed
 
-    if (mainFormIsValid && addressFormIsValid && this.legalNameConfirmed) {
+    if (mainFormIsValid && addressFormIsValid && (!this.editFormShowHide.showName || this.legalNameConfirmed)) {
       // save data from BaseAddress component
       // - only save address if a change was made, ie there is an in-progress address from the component
       if (!Object.values(this.inProgressDelivAddress).every(el => el === undefined)) {
