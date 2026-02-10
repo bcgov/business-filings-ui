10d58aa4dc4dc93d3545b87de6b9d51379d23f61
Lucas <lucasoneil@gmail.com>
2026-02-09 16:28:37 -0800

    Use new PC validators
    Signed-off-by: Lucas <lucasoneil@gmail.com>

--- src/schemas/director-address-schema.ts
+++ src/schemas/director-address-schema.ts
@@ -1,5 +1,5 @@
 import { required, maxLength } from 'vuelidate/lib/validators'
-import { validatePostalCode } from './validators'
+import { isRequiredPostalCode, isValidPostalCode } from '@bcrs-shared-components/validators'
 
 // The Address schema containing Vuelidate rules.
 // NB: This should match the subject JSON schema.
@@ -23,9 +23,9 @@ export const directorAddressSchema = {
     maxLength: maxLength(2)
   },
   postalCode: {
-    required,
+    isRequiredPostalCode,
     maxLength: maxLength(15),
-    validatePostalCode
+    isValidPostalCode
   },
   deliveryInstructions: {
     maxLength: maxLength(80)
